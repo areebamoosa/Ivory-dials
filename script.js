16a6d35e19542f7e28a2158886984bd7deacd6b3
Areeba Moosa <areebamoosa153@gmail.com>
2025-07-20 02:59:25 +0500

    Front Main Page Completed

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// JavaScript Code for Selection of each design of watch
+// Code for Main Header Image Slider
+
+const images = ["Img/banner2.webp", "Img/bann.jpeg", "Img/ban111.jpeg"];
+let index = 0;
+const slideImage = document.getElementById("slideImage");
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+
+  slideImage.classList.add("opacity-0");
+
+  setTimeout(() => {
+    slideImage.src = images[index];
+    slideImage.classList.remove("opacity-0");
+  }, 500);
+}, 2000);
+
+// Code for Selection of each design of watch
 
 const watchBoxes = document.querySelectorAll(".watch-box");
 
