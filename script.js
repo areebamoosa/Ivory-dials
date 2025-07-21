cd48124082e7616f486483cd83611b975e2f6167
Areeba Moosa <areebamoosa153@gmail.com>
2025-07-22 00:27:28 +0500

    Project Completed

--- script.js
+++ script.js
@@ -30,22 +30,40 @@ watchBoxes.forEach((box) => {
   });
 });
 
-// Data Content of each section
+// Code for Active tabs and their content
 
-const tabs = document.querySelectorAll('.tab-button');
-const contents = document.querySelectorAll('.tab-content');
+const tabs = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".tab-content");
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = tab.getAttribute('data-tab');
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = tab.getAttribute("data-tab");
 
-        contents.forEach(content => {
-            if (content.getAttribute('data-content') === target) {
-                content.classList.remove('hidden');
-            } else {
-                content.classList.add('hidden');
-            }
-        });
+    contents.forEach((content) => {
+      content.classList.toggle(
+        "hidden",
+        content.getAttribute("data-content") !== target
+      );
     });
-});
 
+    tabs.forEach((t) => {
+      t.classList.remove(
+        "bg-black",
+        "text-white",
+        "px-10",
+        "py-2",
+        "rounded-tl-xl",
+        "rounded-tr-xl"
+      );
+    });
+
+    tab.classList.add(
+      "bg-black",
+      "text-white",
+      "px-10",
+      "py-2",
+      "rounded-tl-xl",
+      "rounded-tr-xl"
+    );
+  });
+});
